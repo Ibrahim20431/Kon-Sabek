85c855a0e814165c84cc5f3ca7c698f7cb123efa
Ibrahim Shabrawy <76252110+Ibrahim20431@users.noreply.github.com>
2026-04-13 20:58:48 +0200

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -29,7 +29,7 @@ export function Header() {
   <img
     src="/logo.png"
     alt="Logo"
-    className="w-8 h-auto md:w-10 object-contain"
+    className="w-10 h-auto md:w-12 object-contain"
   />
 </a>
           
